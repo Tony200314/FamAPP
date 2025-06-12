e68e21cab86610ef9cd393bd1082a24794536148
Nsangou Tatsinkou Tony Adams <tonytatsinkou@gmail.com>
2025-06-12 14:13:00 +0200

    FAMILY AND Friend App

---
+++ backend/src/app.js
@@ -0,0 +1,22 @@
+const express = require('express'); 
+const cors = require('cors'); 
+const mongoose = require('mongoose') 
+const dotenv = require('dotenv'); 
+const authRoutes = require('./routes/auth');
+const messageRoutes = require('./routes/message');
+
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes); 
+
+app.get('/', (req,res)=>{
+    res.send('API is running');
+});
+
+module.exports = app;
